afbce4f0f43efb58630e34a475561c0e181931ba
Steylor <steylornb@gmail.com>
2023-09-15 07:43:33 -0300

    .

--- sandbox.py
+++ sandbox.py
@@ -14,7 +14,19 @@ for i in range(len(s)):
             validos.append(a)
         t = True
 
+
 if not t:
     validos.append(a)
 
+# valicum = [0]*len(validos)
+
+# valicum[0] = validos[0]
+# for i in range(1,len(validos)):
+#     valicum[i] = validos[i] + valicum[i-1]
+
+for i in range(len(validos)-1, -1,-1):
+    soma += validos[i] * (len(validos) - i)
+
+print(soma)
 print(validos)
+# print(valicum)
