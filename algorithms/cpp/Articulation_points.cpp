2cd1f3dd0589c98f8c2587a28c8f4cecfc39e518
Steylor Nobrega <steylornb@gmail.com>
2024-02-09 22:31:56 -0300

    .

--- algorithms/cpp/Articulation_points.cpp
+++ algorithms/cpp/Articulation_points.cpp
@@ -10,31 +10,31 @@ using namespace std;
 #define all(x) (x).begin(), (x).end()
 ll const mod = 1e9 + 7;
 
-int n;
-vector<vector<int>> adj;
-
+ll n;
+vector<vector<ll>> adj;
 vector<bool> visited;
-vector<int> tin, low;
-int timer;
+vector<ll> tin, low;
+vector<ll> articulation;
+ll timer;
 
-void dfs(int v, int p = -1) {
+void dfs(ll v, ll p = -1) {
     visited[v] = true;
     tin[v] = low[v] = timer++;
-    int children=0;
-    for (int to : adj[v]) {
+    ll children=0;
+    for (ll to : adj[v]) {
         if (to == p) continue;
         if (visited[to]) {
             low[v] = min(low[v], tin[to]);
         } else {
             dfs(to, v);
             low[v] = min(low[v], low[to]);
-            // if (low[to] >= tin[v] && p!=-1)
-            //     IS_CUTPOINT(v);
+            if (low[to] >= tin[v] && p!=-1)
+                articulation.pb(v);
             ++children;
         }
     }
-    // if(p == -1 && children > 1)
-    //     IS_CUTPOINT(v);
+    if(p == -1 && children > 1)
+        articulation.pb(v);
 }
 
 void find_cutpoints() {
@@ -42,7 +42,7 @@ void find_cutpoints() {
     visited.assign(n, false);
     tin.assign(n, -1);
     low.assign(n, -1);
-    for (int i = 0; i < n; ++i) {
+    for (ll i = 0; i < n; ++i) {
         if (!visited[i])
             dfs (i);
     }
---
+++ algorithms/cpp/SCC.cpp
@@ -0,0 +1,63 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define vll vector<long long>
+#define pb push_back
+#define endl "\n"
+#define rep(i, a, b) for(ll i = (a); i < (b); i++)
+#define sorta(arr) sort((arr).begin(), (arr).end())
+#define sz(x) x.size()
+#define all(x) (x).begin(), (x).end()
+ll const mod = 1e9 + 7;
+
+vector<vector<int>> adj, adj_rev;
+vector<bool> used;
+vector<int> order, component;
+
+void dfs1(int v) {
+    used[v] = true;
+
+    for (auto u : adj[v])
+        if (!used[u])
+            dfs1(u);
+
+    order.push_back(v);
+}
+
+void dfs2(int v) {
+    used[v] = true;
+    component.push_back(v);
+
+    for (auto u : adj_rev[v])
+        if (!used[u])
+            dfs2(u);
+}
+
+
+int main(){
+    int n; cin >> n;
+
+    for (;;) {
+        int a, b; cin >> a >> b;
+        adj[a].push_back(b);
+        adj_rev[b].push_back(a);
+    }
+
+    used.assign(n, false);
+
+    for (int i = 0; i < n; i++)
+        if (!used[i])
+            dfs1(i);
+
+    used.assign(n, false);
+    reverse(order.begin(), order.end());
+
+    for (auto v : order)
+        if (!used[v]) {
+            dfs2 (v);
+            
+            component.clear();
+        }
+
+    return 0;
+}
