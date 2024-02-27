257e162020e908623e01f8c2ba43265272a3840f
lesorty <steylornb@gmail.com>
2024-02-27 10:51:51 -0300

    segtree ;-;

--- algorithms/cpp/segtree.cpp
+++ algorithms/cpp/segtree.cpp
@@ -11,36 +11,35 @@ using namespace std;
 ll const mod = 1e9 + 7;
 ll const maxn = 1e5;
 
-ll n;
-ll t[4*maxn];
+ll t[4*maxn+1];
 
-void build(int a[], int v, int tl, int tr) {
+void build(ll a[], ll v, ll tl, ll tr) {
     if (tl == tr) {
         t[v] = a[tl];
     } else {
-        int tm = (tl + tr) / 2;
+        ll tm = (tl + tr) / 2;
         build(a, v*2, tl, tm);
         build(a, v*2+1, tm+1, tr);
         t[v] = t[v*2] + t[v*2+1];
     }
 }
 
-int sum(int v, int tl, int tr, int l, int r) {
+ll sum(ll v, ll tl, ll tr, ll l, ll r) {
     if (l > r) 
         return 0;
     if (l == tl && r == tr) {
         return t[v];
     }
-    int tm = (tl + tr) / 2;
+    ll tm = (tl + tr) / 2;
     return sum(v*2, tl, tm, l, min(r, tm))
            + sum(v*2+1, tm+1, tr, max(l, tm+1), r);
 }
 
-void update(int v, int tl, int tr, int pos, int new_val) {
+void update(ll v, ll tl, ll tr, ll pos, ll new_val) {
     if (tl == tr) {
         t[v] = new_val;
     } else {
-        int tm = (tl + tr) / 2;
+        ll tm = (tl + tr) / 2;
         if (pos <= tm)
             update(v*2, tl, tm, pos, new_val);
         else
@@ -51,7 +50,27 @@ void update(int v, int tl, int tr, int pos, int new_val) {
 
 
 int main () {
+    ll n,m; cin >> n >> m;
 
+    ll arr[n];
+    rep(i,0,n) cin >> arr[i];
+    build(arr, 1, 0, n-1);
+
+
+    rep(i,0,m){
+        ll typ; cin >> typ;
+        if(typ == 1){
+            ll idx, v; cin >> idx >> v;
+
+            update(1, 0, n-1, idx, v);
+        }
+
+        else if(typ ==2){
+            ll l,r; cin >> l >> r;
+
+            cout << sum(1,0,n-1, l, r) << endl;
+        }
+    }
 
     return 0;
 }
