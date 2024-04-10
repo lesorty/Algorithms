a7783ee49e846b624a170aa1ed9a9c37134477fc
lesorty <steylornb@gmail.com>
2024-04-10 14:36:11 -0300

    .

---
+++ algorithms/cpp/matrix-eponetiation.cpp
@@ -0,0 +1,80 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define vll vector<long long>
+#define pb push_back
+#define endl "\n"
+#define rep(i, a, b) for(ll i = (a); i < (b); i++)
+#define sorta(arr) sort((arr).begin(), (arr).end())
+#define sz(x) x.size()
+#define fastio ios::sync_with_stdio(0); cin.tie(0);cout.tie(0)
+#define all(x) (x).begin(), (x).end() 
+ll const mod = 1e9 + 7;
+ll const MAX = 107;
+
+// matrix<int, 2> fib;
+// fib.a = {{ {{1, 1}}, {{1, 0}} }};
+
+template<typename T, int N> struct matrix {
+    array<array<T, N>, N> a{};
+    matrix operator * (const matrix &o) const {
+        matrix ans;
+        rep(i, 0, N) rep(j, 0, N) rep(k, 0, N){
+            ans.a[i][j] += (((a[i][k] % mod) * (o.a[k][j] % mod) % mod));
+            ans.a[i][j] %= mod;
+        }
+        return ans;
+    }
+    matrix operator ^ (ll e) const {
+        matrix ans, self(*this);
+        rep(i, 0, N) ans.a[i][i] = 1;
+        while (e > 0) {
+            if (e & 1) ans = ans * self;
+            self = self * self;
+            e >>= 1;
+        }
+        return ans;
+    }
+    vector<T> operator * (const vector<T> &o) const {
+        vector<T> ans(N);
+        rep(i, 0, N) rep(j, 0, N) {
+            ans[i] += ((a[i][j] % mod) * (o[j] % mod) % mod);
+            ans[i] %= mod;
+        }
+        return ans;
+    }
+};
+
+bool can(ll x, ll y){
+    
+}
+
+
+int main (){
+    fastio;
+
+    ll n,m,k; cin >> n >> m >> k;
+    matrix<ll,9> grid;
+    vector<vector<ll>> adj(65);
+
+    grid.a[64][0] = 1;
+    grid.a[64][64] = 1;
+    
+    rep(i,0,8){
+        rep(j,0,8){
+            if(can (i+2, j+1)) grid.a[i+2][j+1]++;
+            if (can (i+1,j+2)) grid.a[i+1][j+2]++;
+            if(can (i+2,j-1)) grid.a[i+2][j-1]++;
+            if(can (i+1,j-2)) grid.a[i+1][j-2]++;
+            if(can (i-1,j+2)) grid.a[i-1][j+2]++;
+            if(can (i-2,j+1)) grid.a[i-2][j+1]++;
+            if(can(i-2,j-1)) grid.a[i-2][j-1]++;
+            if(can (i-1,j-2)) grid.a[i-1][j-2]++;
+        }
+    }
+
+    
+
+    
+    return 0;
+}
