dbc966d8bc06e88f1281e05a7afac6fc444ee878
Steylor Nobrega <90204277+lesorty@users.noreply.github.com>
2024-08-28 14:53:54 -0300

    Update kmp.cpp

--- algorithms/kmp.cpp
+++ algorithms/kmp.cpp
@@ -1,5 +1,20 @@
+#include <bits/stdc++.h>
+#define endl '\n'
+#define rep(i, begin, end) for (int i = begin; i < end; i++)
+typedef long long ll;
+using namespace std;
+#define pb push_back
+#define all(x) (x).begin(), (x).end()
+#fastio ios::sync_with_stdio(0); cin.tie(0);cout.tie(0)
+const int MOD = 1009;
+const int MAX = 2e5 + 7;
 
 
+int main() {
+    fastio;
+    
+    return 0;
+}
 
 vector<int> kmp(const string &s) {
     int n = s.size();
