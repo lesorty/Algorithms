41033758e7ef45fa1d0e3f17a2faca05bfa82286
Steylor Nobrega <90204277+lesorty@users.noreply.github.com>
2024-08-28 14:53:28 -0300

    Update template.cpp

--- template.cpp
+++ template.cpp
@@ -4,18 +4,14 @@
 typedef long long ll;
 using namespace std;
 #define pb push_back
-#define sz(x) x.size()
 #define all(x) (x).begin(), (x).end()
-#define print(x) cout << x << endl
-#define sz(x) x.size()
-#define input(x) cin >> x
-
+#fastio ios::sync_with_stdio(0); cin.tie(0);cout.tie(0)
 const int MOD = 1009;
 const int MAX = 2e5 + 7;
 
 
 int main() {
-    ios::sync_with_stdio(0); cin.tie(0);cout.tie(0);
+    fastio;
     
     return 0;
 }
