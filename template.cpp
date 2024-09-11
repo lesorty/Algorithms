1e00ac0262296a69eccb70d68b49d6416089738a
Steylor Nobrega <90204277+lesorty@users.noreply.github.com>
2024-09-11 14:46:14 -0300

    Update template.cpp

--- template.cpp
+++ template.cpp
@@ -5,7 +5,7 @@ typedef long long ll;
 using namespace std;
 #define pb push_back
 #define all(x) (x).begin(), (x).end()
-#fastio ios::sync_with_stdio(0); cin.tie(0);cout.tie(0)
+#define fastio ios::sync_with_stdio(0); cin.tie(0);cout.tie(0)
 const int MOD = 1009;
 const int MAX = 2e5 + 7;
 
